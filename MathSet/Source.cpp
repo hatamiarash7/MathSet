167233af42a0895a8e8dfcdc046ce88466144f39
Arash Hatami <hatamiarash7@gmail.com>
2017-05-07 13:20:53 +0430

    First Release

---
+++ MathSet/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+class Set {
+	char set[100];
+	Set() { for (int i = 0; i < 100; i++) set[i] = ' '; }
+public:
+	char* GetMembers() { return set; }
+	Set Union(char* members) {
+		
+	}
+}S1,S2;
+
+void main(void) {
+
+}
