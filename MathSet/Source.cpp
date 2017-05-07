69755819ab99a5e5b1b4bf0144e54a654e6effc6
Arash Hatami <hatamiarash7@gmail.com>
2017-05-07 13:31:41 +0430

    add GetMembersCount()

--- MathSet/Source.cpp
+++ MathSet/Source.cpp
@@ -7,8 +7,15 @@ class Set {
 	Set() { for (int i = 0; i < 100; i++) set[i] = ' '; }
 public:
 	char* GetMembers() { return set; }
-	Set Union(char* members) {
-		
+	int GetMembersCount() {
+		int count = 0;
+		for (int i = 0; i < 100; i++)
+			if (set[i] != ' ')
+				count++;
+		return count;
+	}
+	Set Union(Set S2) {
+		char* S2Members = S2.GetMembers();
 	}
 }S1,S2;
 
